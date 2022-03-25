b5a13f5225184b0b9ede12bea29234e4f5502d69
Pedro84skynet <pedro2.fernandes@usp.br>
2022-03-24 21:37:44 -0300

    added a&d

--- src/attacker.c
+++ src/attacker.c
@@ -18,12 +18,43 @@
 
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
-  UNUSED(defender_spy);
 
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+        static int e1 = 1;
+        static int x = 0;
+        static int y = 0;
+
+        int xp = (int) attacker_position.i;
+        int yp = (int) attacker_position.j;
+
+
+        if (e1) {
+            position_t opponent = get_spy_position(defender_spy);
+            x = (int) opponent.i - xp;
+            if (x > 0) x += 2;
+            else x -= 2;
+            y = (int) opponent.j - yp;
+            e1--;
+        }
+
+        while (x != 0) {
+            if (x < 0) {
+                x++;
+                attacker_position = move_position(attacker_position, DIR_UP);
+                return DIR_UP;
+            } else {
+                x--;
+                attacker_position = move_position(attacker_position, DIR_DOWN);
+                return DIR_DOWN;
+            }
+        }
+
+        while (y != 0) {
+            if (y < 0) y++;
+            else y--;
+            return DIR_RIGHT;
+        }
+
+        return DIR_RIGHT;
 }
 
 /*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -16,14 +16,115 @@
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
-direction_t execute_defender_strategy(
-    position_t defender_position, Spy attacker_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
-  UNUSED(attacker_spy);
+direction_t execute_defender_strategy(position_t defender_position,
+                                      Spy attacker_spy) {
 
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+    static int e1 = 1;
+    static int e2 = 1;
+    static int x = 0;
+    static int y = 0;
+    static int boolean = 0;
+    static int ogx = 0;
+    static int lastx = 0;
+    static int lasty = 0;
+
+    int xp = (int) defender_position.i;
+    int yp = (int) defender_position.j;
+    int change = 0;
+
+    if (lastx == xp && lasty == yp) {
+        change = 1;
+    }
+
+    lastx = xp;
+    lasty = yp;
+
+    if (e1) {
+        position_t opponent = get_spy_position(attacker_spy);
+        ogx = xp;
+        x = (int) opponent.i - xp;
+        y = (int) opponent.j - yp;
+        if (x > 0) boolean = 1;
+        e1 = 0;
+    }
+
+    if ((x || y) == 0) {
+        if ((ogx - xp) > 0) {
+            x = ogx - xp + 2;
+        } else  if ((ogx - xp) < 0) {
+            x = ogx - xp - 2;
+        } else {
+            x = ogx - xp;
+        }
+        e2 = 0;
+    }
+
+    while (x != 0 && e2) {
+        if (y < 0) y++;
+        else y--;
+        if (x < 0) {
+            x++;
+            if (change ) {
+                defender_position = move_position(defender_position, DIR_UP);
+                return DIR_UP;
+            } else {
+                y++;
+                defender_position = move_position(defender_position, DIR_UP_LEFT);
+                return DIR_UP_LEFT;
+            }
+
+        } else {
+            x--;
+            if (change) {
+                defender_position = move_position(defender_position, DIR_DOWN);
+                return DIR_DOWN;
+            } else {
+                y++;
+                defender_position = move_position(defender_position, DIR_DOWN_LEFT);
+                return DIR_DOWN_LEFT;
+            }
+        }
+    }
+
+    while (x != 0 && !e2) {
+        if (x < 0) {
+            x++;
+            if (change) {
+                defender_position = move_position(defender_position, DIR_UP_RIGHT);
+                return DIR_UP_RIGHT;
+            } else {
+                y++;
+                defender_position = move_position(defender_position, DIR_UP);
+                return DIR_UP;
+            }
+
+        } else {
+            x--;
+            if (change) {
+                defender_position = move_position(defender_position, DIR_DOWN_RIGHT);
+                return DIR_DOWN_RIGHT;
+            } else {
+                y++;
+                defender_position = move_position(defender_position, DIR_DOWN);
+                return DIR_DOWN;
+            }
+        }
+    }
+
+    while (y != 0 || e2 == 0) {
+        if (y < 0) y++;
+        else y--;
+        if (boolean) {
+            boolean = 0;
+            defender_position = move_position(defender_position, DIR_UP);
+            return DIR_UP;
+        } else {
+            boolean = 1;
+            defender_position = move_position(defender_position, DIR_DOWN);
+            return DIR_DOWN;
+        }
+    }
+    return DIR_LEFT;
 }
 
 /*----------------------------------------------------------------------------*/
